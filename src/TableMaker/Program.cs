2c49445ec52c82b17463e30a8a7500749e6ca92d
wk-j <somnuk.wk@gmail.com>
2019-03-14 23:47:46 +0700

    Initial commit

---
+++ src/TableMaker/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TableMaker {
+    class Program {
+        static void Main(string[] args) {
+            var data = new[,] {
+                { "C1", "C2" },
+                { "1333", "2333" },
+                { "1333", "23333" },
+                { "ภาษาไทย", "ภาษาไทยมีปัญหาสระลอย" }
+            };
+
+            ArrayPrinter.PrintToConsole(data);
+        }
+    }
+}
